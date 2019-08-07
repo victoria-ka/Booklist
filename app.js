da03929610abb49010adb4cc657bc888dd9fb371
Victoria <victoria.austin1997@gmail.com>
2019-08-07 18:11:48 -0400

    Store book items locally

--- app.js
+++ app.js
@@ -12,20 +12,7 @@ class Book {
 
 class UI {
     static displayBooks() {
-       const StoredBooks = [
-           {
-               title: 'Book One',
-               author: 'John Doe',
-               isbn: '3434434'
-           },
-           {
-               title: 'Book Two',
-               author: 'Jane Doe',
-               isbn: '4550925'
-           }
-       ];
-       
-       const books = StoredBooks;
+       const books = Store.getBooks();
 
        books.forEach(book => UI.addBookToList(book));
     }
@@ -72,6 +59,38 @@ class UI {
 }
 
 //Store Class: Handles storage
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
 
 //Event: Display books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -97,13 +116,27 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
         //Add book to list
         UI.addBookToList(book);
 
+        //Add book to store
+        Store.addBook(book);
+
+        //Alert
+        UI.showAlert('The book has been added!', 'success');
+
         //Clear fields
         UI.clearFields();
     }
 
 });
 
-//Event: Delete a book
+// Event: Delete a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
+    
+    // Remove book from UI
     UI.deleteBook(e.target);
+
+    //Remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+
+    // Alert
+    UI.showAlert('Book Removed', 'success');
 });
