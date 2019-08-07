b6a381661561476412541678f8f1b11c6041cfb6
Victoria <victoria.austin1997@gmail.com>
2019-08-07 15:35:39 -0400

    Create addBookToList method

--- app.js
+++ app.js
@@ -23,13 +23,34 @@ class UI {
                author: 'Jane Doe',
                isbn: '4550925';
            }
-       ] 
+       ];
+       
+       const books = StoredBooks;
+
+       books.forEach(book => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete"X</a></td>
+        `;
+
+        list.appendChild(row);
     }
 }
 
 //Store Class: Handles storage
 
 //Event: Display books
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a book
 
